4ff6c31f656bafd245c32f699d8b7c691058819a
Ivan Kuznetsov <kuzma.wm@gmail.com>
2014-08-18 19:16:05 +0300

    `field()` method implemented

--- src/Wildsurfer/Infusionsoft/Contact.php
+++ src/Wildsurfer/Infusionsoft/Contact.php
@@ -10,8 +10,17 @@ class Contact
     {
     }
 
-    public function field(string $field)
+
+    public function field(string $name)
     {
+        $fields = $this->getConfigFields();
+        if (empty($fields[$name]))
+            throw new SyncException('Field not found in config!');
+
+        if (empty($data[$name]))
+            return null;
+        else
+            return $data[$name];
     }
 
     public function uniqueHash()
