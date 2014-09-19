6f27895d4559dcaff6d16efc4ee6c1ca279385d3
Ivan Kuznetsov <kuzma.wm@gmail.com>
2014-09-19 20:15:19 +0300

    ContactCollection improvement
    - add `diff()` method
    - add `__constructor()` for building collections from array

--- src/Wildsurfer/Infusionsoft/ContactCollection.php
+++ src/Wildsurfer/Infusionsoft/ContactCollection.php
@@ -8,6 +8,22 @@ class ContactCollection
 {
     protected $stack = array();
 
+    public function __construct(array $contacts = array())
+    {
+        foreach ($contacts as $contact) {
+            $this->create($contact);
+        }
+        return $this;
+    }
+
+    public function diff(ContactCollection $collection)
+    {
+        $array = $collection->read();
+        $data = $this->read();
+        $result = array_diff_key($data, $array);
+        return new ContactCollection($result);
+    }
+
     public function create($contact = array())
     {
         if (is_array($contact))
--- tests/Wildsurfer/Tests/Infusionsoft/Unit/ContactCollectionTest.php
+++ tests/Wildsurfer/Tests/Infusionsoft/Unit/ContactCollectionTest.php
@@ -12,6 +12,44 @@ class ContactCollectionTest extends \PHPUnit_Framework_TestCase
 {
 
     /**
+     * `diff()` should return records that are absent in current collection but
+     * present in collection that is passed as argument
+     */
+    public function testDiff()
+    {
+        $c1 = new ContactCollection(array(
+            array(
+                'Id' => 1,
+                'Email' => 'test1@test.com'
+            ),
+            array(
+                'Id' => 2,
+                'Email' => 'test2@test.com'
+            ),
+            array(
+                'Id' => 3,
+                'Email' => 'test3@test.com'
+            )
+        ));
+
+        $c2 = new ContactCollection(array(
+            array(
+                'Id' => 3,
+                'Email' => 'test3@test.com'
+            ),
+            array(
+                'Id' => 4,
+                'Email' => 'test4@test.com'
+            )
+        ));
+
+        $result = $c1->diff($c2);
+
+        $this->assertEquals(2, $result->count());
+
+    }
+
+    /**
      * `read()` function should return array of all contacts from stack. Keys are
      * hashes. For more info see `ContactTest.php` file comments
      */
@@ -25,9 +63,7 @@ class ContactCollectionTest extends \PHPUnit_Framework_TestCase
             'FirstName' => 'FirstName2',
             'Email' => 'test2@test.com'
         );
-        $c = new ContactCollection();
-        $c->create($contact1);
-        $c->create($contact2);
+        $c = new ContactCollection(array($contact1, $contact2));
 
         $expected  = $c->read();
 
@@ -78,9 +114,7 @@ class ContactCollectionTest extends \PHPUnit_Framework_TestCase
             'FirstName' => 'FirstName2',
             'Email' => 'test2@test.com'
         ));
-        $c = new ContactCollection();
-        $c->create($contact1);
-        $c->create($contact2);
+        $c = new ContactCollection(array($contact1, $contact2));
 
         $expected  = $c->count();
         $this->assertEquals($expected, 2);
@@ -100,9 +134,7 @@ class ContactCollectionTest extends \PHPUnit_Framework_TestCase
             'FirstName' => 'FirstName2',
             'Email' => 'test2@test.com'
         );
-        $c = new ContactCollection();
-        $c->create($contact1);
-        $c->create($contact2);
+        $c = new ContactCollection(array($contact1, $contact2));
 
         $expected  = $c->count();
         $this->assertEquals($expected, 2);
